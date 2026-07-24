3e36a8c710089c33f81b3b6734700039ef912160
Arthur Hirata <arthirata2007@gmail.com>
2026-07-24 16:44:39 -0300

    Fiz os erros de input do página de cadastro

--- Front-end/src/App/Layout/Outlet/Cadastros/Cadastros.jsx
+++ Front-end/src/App/Layout/Outlet/Cadastros/Cadastros.jsx
@@ -1,9 +1,65 @@
+import { useState } from "react"
 import styles from "./Cadastros.module.css"
 import SectionTittle from "../../../../Components/Section-Tittle/Section-tittle"
 import Button from "../../../../Components/Button/button"
 function Cadastros(){
-    function onAdicionar(){
-        alert("adcicionar")
+    const userToken = localStorage.getItem("token")
+    const [name, setName] = useState('')
+    const [price, setPrice] = useState('')
+    const [lowAmount, setLowAmount] = useState('')
+    const [image, setImage] = useState('')
+    const [nameError, setNameError] = useState('')
+    const [priceError, setPriceError] = useState('')
+    const [amountError, setAmountError] = useState('')
+    const [imageErro, setImageError] = useState('')
+
+
+
+
+
+
+    const [field, setField] = useState("none")
+
+    async function onAdicionar(){
+        let valid = true
+        if (name.trim === ""){
+            valid = false
+            setNameError('O produto precisa ter um nome')
+        }
+        if (price.trim === ""){
+            valid = false
+            setPriceError('O produto precisa ter um preco estipulado')
+        }
+        if (lowAmount.trim === ""){
+            valid = false
+            setAmountError('O produto precisa ter uma quantidade mínima estipulada')
+        }
+        if (image.trim === ""){
+            valid = false
+            setImageError('O produto precisa ter uma imagem atrelada')
+        }
+        if (!valid){
+            return
+        }
+        const response = await fetch("", {
+            method : 'POST',
+            headers: {
+                    Authorization: `Bearer ${userToken}`,
+            },
+            body : JSON.stringify({
+                name : name,
+                price : price,
+                lowAmount : lowAmount,
+                image : image
+            })
+        })
+        if (response.ok){
+            alert("cadastrado")
+        }
+        else {
+            alert('bug')
+        }
+
     }
     function onRemover(){
         alert("remover")
@@ -19,13 +75,13 @@ function Cadastros(){
                     <span className={styles.cardTittle}>Adicionar Produtos</span>
                     <div className={styles.userAction}>
                         <span className={styles.inputRequest}>Nome do produto</span>
-                        <input type="text" placeholder="Nome produto"/>
+                        <input type="text" placeholder="Nome produto" value={name} onChange={(e) => setName(e.target.value)} className={nameError ? styles.error : ""} /> {nameError && <span className={styles.spanErro}>{nameError}</span>}
                         <span className={styles.inputRequest}>Preço de venda</span>
-                        <input type="text"  placeholder="Preço de venda"/>
+                        <input type="text"  placeholder="Preço de venda" value={price} onChange={(e) => setPrice(e.target.value)}/>
                         <span className={styles.inputRequest}>Quantidade Mínima no estoque</span>
-                        <input type="text" placeholder="Quantidade Mínima estoque" />
+                        <input type="text" placeholder="Quantidade Mínima estoque" value={lowAmount} onChange={(e) => setLowAmount(e.target.value)}  />
                         <span className={styles.inputRequest}>Imagem do produto</span>
-                        <input type="text" placeholder="Url da imagem" />
+                        <input type="text" placeholder="Url da imagem" value={image}  onChange={(e) => setImage(e.target.value)}/>
                     </div>
                     <Button 
                         text="Adicionar"
@@ -48,9 +104,6 @@ function Cadastros(){
                         onClick={onRemover}
                         color="--red"
                     />
-
-
-
                 </div>
                 <div className={styles.cardCadastro}>
                     <span className={styles.cardTittle}>Editar Produto</span>
@@ -61,7 +114,11 @@ function Cadastros(){
                         <input type="text" placeholder="ID produto"/>
                         <span className={styles.inputRequest}>Edição</span>
                         <input type="text" placeholder="Digite a edição" />
-                    <select name="" className={styles.select}>
+                    <select 
+                        value={field}
+                        onChange={(e) => setField(e.target.value)}
+                    
+                    className={styles.select}>
                         <option value="none" disabled selected hidden>Selecione</option>
                         <option value="name">Nome do produto</option>
                         <option value="price">Preço do produto</option>
