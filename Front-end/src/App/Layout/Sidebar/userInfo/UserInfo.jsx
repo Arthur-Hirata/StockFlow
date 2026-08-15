ff5eb85d47b6f9ab598772225fe4c19e17d253ed
Arthur Hirata <arthirata2007@gmail.com>
2026-08-15 16:37:19 -0300

    Finalizei a funcionalidade do botão de deslogar

--- Front-end/src/App/Layout/Sidebar/userInfo/UserInfo.jsx
+++ Front-end/src/App/Layout/Sidebar/userInfo/UserInfo.jsx
@@ -1,11 +1,13 @@
 
 import { useState } from "react"
 import styles from "./Userinfo.module.css"
-
+import ConfirmartionModal from "../../../../Components/ConfirmationModal/ConfirmationModal"
+import { useNavigate } from "react-router-dom"
 function Userinfo({id, nome, role}){
     const [isOpen, setIsOpen] = useState(false)
     const [isClosing, setIsClosing] = useState(false)
-    
+    const [ConfirmationModal, setConfirmModal] = useState("")
+    const navigate = useNavigate()
     const handleToggle = () =>{
         if (isOpen){
             setIsClosing(true)
@@ -19,7 +21,20 @@ function Userinfo({id, nome, role}){
         setIsClosing(false)
     }
 
+    function logOut(){
+        setConfirmModal({
+            content : "sair da sua conta?",
+            text : "Sair",
+            color1: "--green",
+            color2: "--red",
+            onConfirm : async() =>{
+                setConfirmModal(null)
+                localStorage.setItem("token", "")
+                navigate('/Login')
 
+            }
+        })
+    }
     return(
         <div className={styles.containerUserInfo} >
             <div className={styles.displayContainer} 
@@ -43,9 +58,15 @@ function Userinfo({id, nome, role}){
                     <p>ID: {id}</p>
                     <p>Nome: {nome}</p>
                     <p>Role: {role}</p>
-                    <button className={styles.btnSair}>Sair <i className=""></i></button>
+                    <button className={styles.btnSair} onClick={logOut}>Sair <i className=""></i></button>
                 </div>
             )}
+            {ConfirmationModal && <ConfirmartionModal onClose={()=>setConfirmModal(null)} 
+                    onConfirm={ConfirmationModal.onConfirm}
+                    content={ConfirmationModal.content} 
+                    color1={ConfirmationModal.color1} 
+                    color2={ConfirmationModal.color2}   
+                    text={ConfirmationModal.text}   />}
         </div>
     )
 }
